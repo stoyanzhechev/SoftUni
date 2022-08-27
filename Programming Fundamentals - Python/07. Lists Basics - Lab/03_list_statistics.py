fb0ac2c6321e5dce192aee2384121fb2d399fc6a
stoyanzhechev <marbulk@gmail.com>
2022-08-27 22:27:05 +0300

    initial commit

--- Programming Fundamentals - Python/07. Lists Basics - Lab/03_list_statistics.py
+++ Programming Fundamentals - Python/07. Lists Basics - Lab/03_list_statistics.py
@@ -1,19 +1,15 @@
 n = int(input())
 
-positive_counter = 0
-negative_sum = 0
 positive_numbers_list = []
 negative_numbers_list = []
 
-for number in range(n):
+for row in range(n):
     current_number = int(input())
     if current_number >= 0:
         positive_numbers_list.append(current_number)
-        positive_counter += 1
     else:
         negative_numbers_list.append(current_number)
-        negative_sum += current_number
 
 print(positive_numbers_list)
 print(negative_numbers_list)
-print(f'Count of positives: {positive_counter} \nSum of negatives: {negative_sum}')
+print(f'Count of positives: {len(positive_numbers_list)} \nSum of negatives: {sum(negative_numbers_list)}')
