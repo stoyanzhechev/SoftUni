1fd1cd58fbbc8936f00196ef96ca69f0a98737d0
stoyanzhechev <marbulk@gmail.com>
2022-08-30 00:55:43 +0300

    initial commit

---
+++ Programming Basics - Python/16. Basics Module Problems A to Z/7_projects_creation.py
@@ -0,0 +1,6 @@
+architect_name = input()
+projects_count = int(input())
+
+required_time = projects_count * 3
+
+print(f"The architect {architect_name} will need {required_time} hours to complete {projects_count} project/s.")
