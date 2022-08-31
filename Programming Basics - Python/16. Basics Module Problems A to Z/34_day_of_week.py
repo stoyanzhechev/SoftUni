70fcf88b8880116bda8c1c4198b54535e0a54348
stoyanzhechev <marbulk@gmail.com>
2022-08-31 16:43:33 +0300

    initial commit

---
+++ Programming Basics - Python/16. Basics Module Problems A to Z/34_day_of_week.py
@@ -0,0 +1,18 @@
+input_number = int(input())
+
+if input_number == 1:
+    print('Monday')
+elif input_number == 2:
+    print('Tuesday')
+elif input_number == 3:
+    print('Wednesday')
+elif input_number == 4:
+    print('Thursday')
+elif input_number == 5:
+    print('Friday')
+elif input_number == 6:
+    print('Saturday')
+elif input_number == 7:
+    print('Sunday')
+else:
+    print('Error')
