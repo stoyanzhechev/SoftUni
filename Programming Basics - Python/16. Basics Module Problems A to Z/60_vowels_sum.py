947b2fa74411edc3471092b5f4daa820be551606
stoyanzhechev <marbulk@gmail.com>
2022-09-01 17:00:32 +0300

    initial commit

---
+++ Programming Basics - Python/16. Basics Module Problems A to Z/60_vowels_sum.py
@@ -0,0 +1,18 @@
+input_string = input()
+vowels_sum = 0
+
+for char in input_string:
+    if char == 'a':
+        vowels_sum += 1
+    elif char == 'e':
+        vowels_sum += 2
+    elif char == 'i':
+        vowels_sum += 3
+    elif char == 'o':
+        vowels_sum += 4
+    elif char == 'u':
+        vowels_sum += 5
+
+print(vowels_sum)
+
+
