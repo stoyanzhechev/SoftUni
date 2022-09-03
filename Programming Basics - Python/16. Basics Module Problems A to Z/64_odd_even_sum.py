9efbb92c6d0642ba0bf7a8bce9cf68f65f980e74
stoyanzhechev <marbulk@gmail.com>
2022-09-03 13:54:12 +0300

    initial commit

---
+++ Programming Basics - Python/16. Basics Module Problems A to Z/64_odd_even_sum.py
@@ -0,0 +1,18 @@
+number_count = int(input())
+
+odd_sum = 0
+even_sum = 0
+
+for i in range(number_count):
+    current_number = int(input())
+    if i % 2 == 0:
+        even_sum += current_number
+    else:
+        odd_sum += current_number
+
+if even_sum == odd_sum:
+    print(f'Yes \nSum = {even_sum}')
+else:
+    diff = abs(even_sum - odd_sum)
+    print(f'No \nDiff = {diff}')
+
