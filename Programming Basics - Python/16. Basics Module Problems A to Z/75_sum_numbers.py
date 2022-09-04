022d22faa6b915962a0541ca31a04cb068958ecf
stoyanzhechev <marbulk@gmail.com>
2022-09-04 19:42:19 +0300

    initial commit

--- Programming Basics - Python/16. Basics Module Problems A to Z/75_sum_numbers.py
+++ Programming Basics - Python/16. Basics Module Problems A to Z/75_sum_numbers.py
@@ -1,12 +1,10 @@
 initial_number = int(input())
-
-current_number = int(input())
 total_sum = 0
+
 while True:
+    current_number = int(input())
     total_sum += current_number
 
     if total_sum >= initial_number:
         break
-    current_number = int(input())
-
 print(total_sum)
