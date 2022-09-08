fa96ec2a8d6829a815362b8c6b8b6df9ef508e3c
stoyanzhechev <marbulk@gmail.com>
2022-09-08 12:10:04 +0300

    initial commit

--- Programming Basics - Python/16. Basics Module Problems A to Z/76_sequence_2k_plus_1.py
+++ Programming Basics - Python/16. Basics Module Problems A to Z/76_sequence_2k_plus_1.py
@@ -2,6 +2,8 @@ input_number = int(input())
 
 current_num = 1
 
-while current_num <= input_number:
+while True:
     print(current_num)
     current_num = current_num * 2 + 1
+    if current_num > input_number:
+        break
