9a116fed6f1735384a8677031b2a5ae049aa71b0
stoyanzhechev <marbulk@gmail.com>
2022-09-13 07:53:23 +0300

    initial commit

--- Programming Basics - Python/16. Basics Module Problems A to Z/93_building.py
+++ Programming Basics - Python/16. Basics Module Problems A to Z/93_building.py
@@ -3,5 +3,11 @@ rooms = int(input())
 
 for f in range(floors, 0, -1):
     for r in range(rooms):
-        print(f'{f}{r}', end=' ')
+        if f == floors:
+            print(f'L{f}{r}', end=' ')
+        elif f % 2 == 0:
+            print(f'O{f}{r}', end=' ')
+        else:
+            print(f'A{f}{r}', end=' ')
+
     print()
