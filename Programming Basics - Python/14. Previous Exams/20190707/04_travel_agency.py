98bd4d8e12d4226b953ea19f4a89a53fbf286d53
stoyanzhechev <marbulk@gmail.com>
2022-09-15 20:12:13 +0300

    initial commit

--- Programming Basics - Python/14. Previous Exams/20190707/04_travel_agency.py
+++ Programming Basics - Python/14. Previous Exams/20190707/04_travel_agency.py
@@ -2,42 +2,56 @@ town = input()
 package = input()
 vip_discount = input()
 stay_days = int(input())
-is_valid = True
+invalid_input = False
 
 price = 0
+discount = 0
 
 if town == 'Bansko' or town == 'Borovets':
     if package == 'withEquipment':
         price = 100
         if vip_discount == 'yes':
-            price *= 0.90
+            discount = 10
+        else:
+            discount = 0
     elif package == 'noEquipment':
         price = 80
         if vip_discount == 'yes':
-            price *= 0.95
+            discount = 5
+        else:
+            discount = 0
+    else:
+        invalid_input = True
 elif town == 'Varna' or town == 'Burgas':
     if package == 'withBreakfast':
         price = 130
         if vip_discount == 'yes':
-            price *= 0.88
+            discount = 12
+        else:
+            discount = 0
     elif package == 'noBreakfast':
         price = 100
         if vip_discount == 'yes':
-            price *= 0.93
+            discount = 7
+        else:
+            discount = 0
+    else:
+        invalid_input = True
 else:
-    is_valid = False
+    invalid_input = True
 
-total_cost = stay_days * price
+total_price_per_day = price * (100 - discount) / 100
+total_price = total_price_per_day * stay_days
 
-if not is_valid:
+if stay_days > 7:
+    total_price -= total_price_per_day
+
+if stay_days >= 1 and not invalid_input:
+    print(f"The price is {total_price:.2f}lv! Have a nice time!")
+elif stay_days < 1 and not invalid_input:
+    print("Days must be positive number!")
+else:
     print("Invalid input!")
-else:
-    if stay_days > 1:
-        print(f"The price is {total_cost:.2f}lv! Have a nice time!")
-    elif stay_days < 1:
-        print("Days must be positive number!")
-
-
 
 
 
