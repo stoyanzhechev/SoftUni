d2d7eb2e5ba840c4883cb3877412640515762bc3
stoyan.zhechev <marbulk@gmail.com>
2022-09-16 14:58:27 +0300

    initial commit

--- Programming Fundamentals - Python/01. Basic Syntax, Conditional Statements and Loops - Lab/01_number_definer.py
+++ Programming Fundamentals - Python/01. Basic Syntax, Conditional Statements and Loops - Lab/01_number_definer.py
@@ -3,14 +3,14 @@ number = float(input())
 if number == 0:
     print('zero')
 elif number > 0:
-    if 0 < number < 1:
+    if number < 1:
         print('small positive')
     elif number > 1000000:
         print('large positive')
     else:
         print('positive')
 elif number < 0:
-    if -1 < number < 0:
+    if number > -1:
         print('small negative')
     elif number < -1000000:
         print('large negative')
