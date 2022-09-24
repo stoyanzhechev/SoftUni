e6a3af6576047adb7c8a08e790cf70532a897029
stoyanzhechev <marbulk@gmail.com>
2022-09-24 08:04:31 +0300

    initial commit

--- Programming Fundamentals - Python/02. Basic Syntax, Conditional Statements and Loops - Exercise/11_easter_bread.py
+++ Programming Fundamentals - Python/02. Basic Syntax, Conditional Statements and Loops - Exercise/11_easter_bread.py
@@ -1,17 +1,18 @@
 budget = float(input())
 flour_price_kg = float(input())
 eggs_price_pack = flour_price_kg * 0.75
-milk_price_l = flour_price_kg * 1.25
-milk_price_ml = milk_price_l / 4
+milk_price_ltr = flour_price_kg * 1.25
+milk_price_per_loaf = milk_price_ltr / 4
+total_price_per_loaf = flour_price_kg + eggs_price_pack + milk_price_per_loaf
+loaves_count = 0
+colored_eggs_count = 0
 
-colored_eggs = 0
-loaf_count = 0
+while budget >= total_price_per_loaf:
+    budget -= total_price_per_loaf
+    loaves_count += 1
+    colored_eggs_count += 3
+    if loaves_count % 3 == 0:
+        colored_eggs_count -= (loaves_count - 2)
 
-while budget > (flour_price_kg + eggs_price_pack + milk_price_ml):
-    budget -= (flour_price_kg + eggs_price_pack + milk_price_ml)
-    loaf_count += 1
-    colored_eggs += 3
-    if loaf_count % 3 == 0:
-        colored_eggs -= (loaf_count - 2)
-
-print(f"You made {loaf_count} loaves of Easter bread! Now you have {colored_eggs} eggs and {budget:.2f}BGN left.")
+print(f"You made {loaves_count} loaves of Easter bread! Now you have \
+{colored_eggs_count} eggs and {budget:.2f}BGN left.")
--- Programming Fundamentals - Python/temp.py
+++ Programming Fundamentals - Python/temp.py
@@ -1,11 +1,19 @@
-first_string = input()
-second_string = input()
-last_printed_string = first_string
-for character_index in range(len(first_string)):
-    left_part = second_string[:character_index + 1]
-    right_part = first_string[character_index + 1:]
-    new_string = left_part + right_part
-    if new_string == last_printed_string:
-        continue
-    print(new_string)
-    last_printed_string = new_string
+budget = float(input())
+flour_price_kg = float(input())
+eggs_price_pack = flour_price_kg * 0.75
+milk_price_ltr = flour_price_kg * 1.25
+milk_price_per_loaf = milk_price_ltr / 4
+total_price_per_loaf = flour_price_kg + eggs_price_pack + milk_price_per_loaf
+loaves_count = 0
+colored_eggs_count = 0
+
+while budget >= total_price_per_loaf:
+    budget -= total_price_per_loaf
+    loaves_count += 1
+    colored_eggs_count += 3
+    if loaves_count % 3 == 0:
+        colored_eggs_count -= (loaves_count - 2)
+
+print(f"You made {loaves_count} loaves of Easter bread! Now you have \
+{colored_eggs_count} eggs and {budget:.2f}BGN left.")
+
