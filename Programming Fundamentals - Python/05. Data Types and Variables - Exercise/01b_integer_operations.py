8115f8b99024dfb773225cf06afd0476d5fdc541
stoyanzhechev <marbulk@gmail.com>
2022-09-28 07:51:34 +0300

    initial commit

---
+++ Programming Fundamentals - Python/05. Data Types and Variables - Exercise/01b_integer_operations.py
@@ -0,0 +1,5 @@
+first_number, second_number, third_number, forth_number = int(input()), int(input()), int(input()), int(input())
+first_result = first_number + second_number
+second_result = int(first_result / third_number)
+final_result = second_result * forth_number
+print(final_result)
