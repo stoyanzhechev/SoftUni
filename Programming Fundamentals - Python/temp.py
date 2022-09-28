a4f3ca01743c4fb9019e89593c55ef8aa12fa378
stoyanzhechev <marbulk@gmail.com>
2022-09-28 07:50:50 +0300

    initial commit

--- Programming Fundamentals - Python/temp.py
+++ Programming Fundamentals - Python/temp.py
@@ -1,4 +1,5 @@
-gbp_amount = int(input())
-usd_amount = gbp_amount * 1.31
-
-print(f'{usd_amount:.3f}')
+first_number, second_number, third_number, forth_number = int(input()), int(input()), int(input()), int(input())
+first_result = first_number + second_number
+second_result = int(first_result / third_number)
+final_result = second_result * forth_number
+print(final_result)
