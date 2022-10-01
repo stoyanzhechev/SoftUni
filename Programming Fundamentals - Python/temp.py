76a3a2ee7892eac0c304a31944d37999661a7a6c
stoyanzhechev <marbulk@gmail.com>
2022-10-01 08:27:20 +0300

    initial commit

--- Programming Fundamentals - Python/temp.py
+++ Programming Fundamentals - Python/temp.py
@@ -1,12 +1,9 @@
-number_of_people = int(input())
-elevator_capacity = int(input())
-number_of_courses = 0
+number_of_lines = int(input())
+sum_of_chars = 0
 
-while number_of_people >= 0:
-    if number_of_people > elevator_capacity:
-        number_of_people -= elevator_capacity
-        number_of_courses += 1
-    else:
-        number_of_courses += 1
-        break
-print(number_of_courses)
+for char in range(number_of_lines):
+    char = input()
+    ascii_number = ord(char)
+    sum_of_chars += ascii_number
+
+print(f'The sum equals: {sum_of_chars}')
