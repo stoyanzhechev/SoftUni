8718278325ce371be48b1a9ee151c6352f6049e1
stoyanzhechev <marbulk@gmail.com>
2022-10-01 10:40:10 +0300

    initial commit

--- Programming Fundamentals - Python/temp.py
+++ Programming Fundamentals - Python/temp.py
@@ -1,12 +1,21 @@
-number_of_pours = int(input())
-filled_capacity = 0
-sufficient_capacity = True
+group_size = int(input())
+adventure_days = int(input())
+days_counter = 0
+total_coins = 0
 
-for i in range(number_of_pours):
-    current_pour = int(input())
-    if (255 - filled_capacity) < current_pour:
-        sufficient_capacity = False
-        print('Insufficient capacity!')
-    else:
-        filled_capacity += current_pour
-print(filled_capacity)
+for current_day in range(1, adventure_days + 1):
+    days_counter += 1
+    if days_counter % 10 == 0:
+        group_size -= 2
+    if days_counter % 15 == 0:
+        group_size += 5
+    total_coins += 50 - (group_size * 2)
+    if days_counter % 3 == 0:
+        total_coins -= group_size * 3
+    if days_counter % 5 == 0:
+        total_coins += group_size * 20
+        if days_counter % 3 == 0:
+            total_coins -= group_size * 2
+coins_per_party_member = int(total_coins / group_size)
+
+print(f"{group_size} companions received {coins_per_party_member} coins each.")
