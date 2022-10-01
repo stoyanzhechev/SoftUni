4b3ff57dd1bce170024753711a166a93d78484e3
stoyanzhechev <marbulk@gmail.com>
2022-10-01 10:47:09 +0300

    initial commit

--- Programming Fundamentals - Python/05. Data Types and Variables - Exercise/07b_water_overflow.py
+++ Programming Fundamentals - Python/05. Data Types and Variables - Exercise/07b_water_overflow.py
@@ -1,11 +1,9 @@
 number_of_pours = int(input())
 filled_capacity = 0
-sufficient_capacity = True
 
 for i in range(number_of_pours):
     current_pour = int(input())
     if (255 - filled_capacity) < current_pour:
-        sufficient_capacity = False
         print('Insufficient capacity!')
     else:
         filled_capacity += current_pour
