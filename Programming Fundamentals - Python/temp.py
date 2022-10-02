e0a09807b210a5d561ecdbba702dc4549217f955
stoyanzhechev <marbulk@gmail.com>
2022-10-02 09:25:02 +0300

    initial commit

--- Programming Fundamentals - Python/temp.py
+++ Programming Fundamentals - Python/temp.py
@@ -1,14 +1,8 @@
-lost_fights_count = int(input())
-helmet_price = float(input())
-sword_price = float(input())
-shield_price = float(input())
-armor_price = float(input())
+tail = input()
+body = input()
+head = input()
 
-helmet_total_price = (lost_fights_count // 2) * helmet_price
-sword_total_price = (lost_fights_count // 3) * sword_price
-shield_total_price = (lost_fights_count // 6) * shield_price
-armor_total_price = (lost_fights_count // 12) * armor_price
+meercat = [head, body, tail]
 
-total_expenses = helmet_total_price + sword_total_price + shield_total_price + armor_total_price
+print(meercat)
 
-print(f"Gladiator expenses: {total_expenses:.2f} aureus")
