e3b5e4ad02ddf0120a720e3832f681f14eb1afd2
stoyanzhechev <marbulk@gmail.com>
2022-10-05 23:45:57 +0300

    initial commit

---
+++ Programming Fundamentals - Python/temp.py
@@ -0,0 +1,7 @@
+list_of_numbers = input().split()
+opposite_numbers_list = []
+for element in list_of_numbers:
+    current_number = -int(element)
+    opposite_numbers_list.append(current_number)
+
+print(opposite_numbers_list)
