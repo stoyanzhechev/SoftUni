e9f777e9e76ec481359bdeadd549b4d96c11f165
stoyanzhechev <marbulk@gmail.com>
2022-10-07 07:55:49 +0300

    initial commit

--- Programming Fundamentals - Python/temp.py
+++ Programming Fundamentals - Python/temp.py
@@ -1,9 +1,1 @@
-factor = int(input())
-count = int(input())
-list_of_numbers = []
 
-for i in range(1, count + 1):
-    current_number = factor * i
-    list_of_numbers.append(current_number)
-
-print(list_of_numbers)
