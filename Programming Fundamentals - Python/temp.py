8d4c6ef85eeb7a01943bfb72b1ff4550548e3158
stoyanzhechev <marbulk@gmail.com>
2022-10-09 17:58:40 +0300

    initial commit

--- Programming Fundamentals - Python/temp.py
+++ Programming Fundamentals - Python/temp.py
@@ -1,41 +1,17 @@
-collection_list = input(). split('|')
-budget = float(input())
-bought_items_price_list = []
-profit = 0
+initial_loot_list = input().split('|')
+command = input()
+final_loot_list = initial_loot_list
 
-for element in collection_list:
-    bought_item = element.split('->')
-    item_type = bought_item[0]
-    item_value = float(bought_item[1])
-    if budget < item_value:
-        break
-    if item_type == 'Clothes' and item_value <= 50.00:
-        budget -= item_value
-        sold_price = item_value * 1.40
-        profit += sold_price - item_value
-    elif item_type == 'Shoes' and item_value <= 35.00:
-        budget -= item_value
-        sold_price = item_value * 1.40
-        profit += sold_price - item_value
-    elif item_type == 'Accessories' and item_value <= 20.50:
-        budget -= item_value
-        sold_price = item_value * 1.40
-        profit += sold_price - item_value
-    else:
-        continue
-    bought_items_price_list.append(round(sold_price, 2))
+while command != 'Yohoho!':
+    command_action = command.split()
+    if command_action[0] == 'Loot':
+        pass
+    elif command_action[0] == 'Drop':
+        if int(command_action[1]) in initial_loot_list:
+            initial_loot_list.pop[int(command_action[1])]
+            dropped_item = initial_loot_list.pop[int(command_action[1])]
+            initial_loot_list.append(dropped_item)
+    elif command_action[0] == 'Steal':
+        pass
 
-
-final_bought_list = ' '.join(map(str, bought_items_price_list))
-print(final_bought_list)
-print(f'Profit: {profit:.2f}')
-
-new_budget = budget + sum(bought_items_price_list)
-
-if new_budget >= 150:
-    print('Hello, France!')
-else:
-    print('Not enough money.')
-
-
-
+    command= input()
