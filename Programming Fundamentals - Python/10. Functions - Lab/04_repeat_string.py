f1df09f7ef39dd5fd0046694843b85f0c58b5015
stoyanzhechev <marbulk@gmail.com>
2022-10-11 22:09:26 +0300

    initial commit

--- Programming Fundamentals - Python/10. Functions - Lab/04_repeat_string.py
+++ Programming Fundamentals - Python/10. Functions - Lab/04_repeat_string.py
@@ -1,1 +1,7 @@
-def repeat_string(some_string):
+input_string = input()
+number_of_repetitions = int(input())
+
+output_string = lambda a, b: a * b
+result = output_string(input_string, number_of_repetitions)
+
+print(result)
