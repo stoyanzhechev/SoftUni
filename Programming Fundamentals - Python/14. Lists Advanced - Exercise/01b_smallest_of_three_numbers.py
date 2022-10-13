ebc8767a9fd6b1d079bac32290dc2e39f15433fa
stoyanzhechev <marbulk@gmail.com>
2022-10-13 07:54:54 +0300

    initial commit

--- Programming Fundamentals - Python/14. Lists Advanced - Exercise/01b_smallest_of_three_numbers.py
+++ Programming Fundamentals - Python/14. Lists Advanced - Exercise/01b_smallest_of_three_numbers.py
@@ -1,10 +1,10 @@
 def smallest_number(numbers):
-    min_number = min(numbers)
-    return min_number
+    return min(numbers)
 
 
 first_number = int(input())
 second_number = int(input())
 third_number = int(input())
 all_numbers = [first_number, second_number, third_number]
-print(smallest_number(all_numbers))
+min_number = smallest_number((all_numbers))
+print(min_number)
