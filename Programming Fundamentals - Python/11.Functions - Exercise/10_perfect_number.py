b9d734f1ee7f2c1008518a3ecb75ec208dcba8b2
stoyanzhechev <marbulk@gmail.com>
2022-10-14 21:22:06 +0300

    initial commit

---
+++ Programming Fundamentals - Python/11.Functions - Exercise/10_perfect_number.py
@@ -0,0 +1,12 @@
+def perfect_number(some_number):
+    sum = 0
+    for divisor in range(1, some_number):
+        if some_number % divisor == 0:
+            sum += divisor
+    if sum == some_number:
+        return "We have a perfect number!"
+    return "It's not so perfect."
+
+
+number = int(input())
+print(perfect_number(number))
