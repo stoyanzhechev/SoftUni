c4f7ee940b65a9af0e58f86806ea08812f297190
stoyanzhechev <marbulk@gmail.com>
2022-10-15 11:08:05 +0300

    initial commit

---
+++ Programming Fundamentals - Python/13. Lists Advanced - Lab/01b_no_vowels.py
@@ -0,0 +1,7 @@
+input_string = input()
+vowels_list = ['a', 'o', 'u', 'e', 'i']
+result = []
+for char in input_string:
+    if char not in vowels_list:
+        result.append(char)
+print(''.join(result))
