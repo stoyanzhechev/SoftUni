ee63630268c776a572cc0e62d20ae24eb483916c
stoyanzhechev <marbulk@gmail.com>
2022-10-22 20:02:58 +0300

    initial commit

---
+++ Programming Fundamentals - Python/Exam Problems/03. Mid Exam/03_moving_target.py
@@ -0,0 +1,29 @@
+targets_list = list(map(int, input().split()))
+command = input()
+
+while command != 'End':
+    command = command.split()
+    command_action = command[0]
+    command_index = int(command[1])
+    if command_action == 'Shoot':
+        if 0 <= command_index < len(targets_list):
+            command_power = int(command[2])
+            targets_list[command_index] -= command_power
+            if targets_list[command_index] <= 0:
+                targets_list.remove(targets_list[command_index])
+    elif command_action == 'Add':
+        command_value = int(command[2])
+        if 0 <= command_index < len(targets_list):
+            targets_list.insert(command_index, command_value)
+        else:
+            print('Invalid placement!')
+    elif command_action == 'Strike':
+        command_radius = int(command[2])
+        if 0 <= command_index - command_radius < len(targets_list) and 0 <= command_index + command_radius < len(targets_list):
+            targets_list.remove(targets_list[(command_index - command_radius):(command_index + command_radius)])
+        else:
+            print('Strike missed')
+    command = input()
+
+targets_list_converted_to_string = [str(num) for num in targets_list]
+print('|'.join(targets_list_converted_to_string))
