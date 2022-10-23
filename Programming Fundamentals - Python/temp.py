9026c716c2cd9fe6faca6c0ae9cd82e96b14a056
stoyanzhechev <marbulk@gmail.com>
2022-10-23 10:15:25 +0300

    initial commit

--- Programming Fundamentals - Python/temp.py
+++ Programming Fundamentals - Python/temp.py
@@ -1,24 +1,25 @@
-needed_experience = float(input())
-battles_count = int(input())
-gained_experience = 0
-experience_gathered = False
+coffee_list = input().split()
+commands_number = int(input())
 
-for battle in range(1, battles_count + 1):
-    current_battle_experience = float(input())
-    if battle % 3 == 0:
-        current_battle_experience += 0.15 * current_battle_experience
-    if battle % 5 == 0:
-        current_battle_experience -= 0.10 * current_battle_experience
-    if battle % 15 == 0:
-        current_battle_experience += 0.05 * current_battle_experience
-    gained_experience += current_battle_experience
-    if gained_experience >= needed_experience:
-        experience_gathered = True
-        break
+for commands in range(0, commands_number):
+    command = input().split()
+    command_action = command[0]
+    if command_action == 'Include':
+        coffee_list.append(command[1])
+    elif command_action == 'Remove':
+        number_of_coffees_to_remove = int(command[2])
+        if command[1] == 'first':
+            del coffee_list[:number_of_coffees_to_remove]
+        elif command[1] == 'last':
+            del coffee_list[len(coffee_list) - number_of_coffees_to_remove:]
+    elif command_action == 'Prefer':
+        first_coffee_index = int(command[1])
+        second_coffee_index = int(command[2])
+        if first_coffee_index < len(coffee_list) and second_coffee_index < len(coffee_list):
+            coffee_list[first_coffee_index], coffee_list[second_coffee_index] = \
+            coffee_list[second_coffee_index], coffee_list[first_coffee_index]
+    elif command_action == 'Reverse':
+        coffee_list.reverse()
 
-if experience_gathered:
-    print(f'Player successfully collected his needed experience for {battle} battles.')
-else:
-    diff = abs(needed_experience - gained_experience)
-    print(f'Player was not able to collect the needed experience, {diff:.2f} more needed. ')
-
+print('Coffees:')
+print(' '.join(coffee_list))
