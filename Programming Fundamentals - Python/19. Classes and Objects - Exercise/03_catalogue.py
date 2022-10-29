f722c97a0e83b956de62554833ecf05c215bec5e
stoyanzhechev <marbulk@gmail.com>
2022-10-29 13:43:54 +0300

    initial commit

---
+++ Programming Fundamentals - Python/19. Classes and Objects - Exercise/03_catalogue.py
@@ -0,0 +1,17 @@
+class Catalogue:
+    def __init__(self, name):
+        self.name = name
+        self.products = []
+
+    def add_product(self, product_name):
+        self.products.append(product_name)
+
+    def get_by_letter(self, first_letter):
+        return [product for product in self.products if product.startswith(first_letter)]
+
+    def __repr__(self):
+        string_for_return = f"Items in the {self.name} catalogue:\n"
+        string_for_return += "\n".join(sorted(self.products))
+        return string_for_return
+
+
