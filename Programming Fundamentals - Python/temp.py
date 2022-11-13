9de9f41e4ab887a8e43f85e082a43c33cd26a078
stoyanzhechev <marbulk@gmail.com>
2022-11-13 13:00:13 +0200

    initial commit

--- Programming Fundamentals - Python/temp.py
+++ Programming Fundamentals - Python/temp.py
@@ -1,13 +1,16 @@
-resources = {}
-command = input()
+phonebook = {}
 
-while command != 'stop':
-    current_resource = command
-    current_quantity = int(input())
-    if current_resource not in resources.keys():
-        resources[current_resource] = 0
-    resources[current_resource] += current_quantity
-    command = input()
+while True:
+    current_input = input()
+    if '-' not in current_input:
+        break
+    name, phone = current_input.split('-')
+    phonebook[name] = phone
+for check in range(int(current_input)):
+    searched_name = input()
+    if searched_name in phonebook.keys():
+        found_number = phonebook[searched_name]
+        print(f"{searched_name} -> {found_number}")
+    else:
+        print(f"Contact {searched_name} does not exist.")
 
-for resource, quantity in resources.items():
-    print(f"{resource} -> {quantity}")
