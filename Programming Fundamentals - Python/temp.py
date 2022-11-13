0ebb81bdde44368b40cf6cbf314bfb938b4eb348
stoyanzhechev <marbulk@gmail.com>
2022-11-13 13:22:26 +0200

    initial commit

--- Programming Fundamentals - Python/temp.py
+++ Programming Fundamentals - Python/temp.py
@@ -1,16 +1,35 @@
-phonebook = {}
+items_dictionary = {'shards': 0, 'fragments': 0, 'motes': 0}
+current_items = input().split()
+legendary_is_obtained = False
 
-while True:
-    current_input = input()
-    if '-' not in current_input:
+while not legendary_is_obtained:
+    for index in range(0, len(current_items), 2):
+        value = int(current_items[index])
+        key = current_items[index + 1].lower()
+        if key not in items_dictionary.keys():
+            items_dictionary[key] = 0
+        items_dictionary[key] += value
+        if items_dictionary['shards'] >= 250:
+            legendary_is_obtained = True
+            items_dictionary['shards'] -= 250
+            print('Shadowmourne obtained!')
+        elif items_dictionary['fragments'] >= 250:
+            legendary_is_obtained = True
+            items_dictionary['fragments'] -= 250
+            print('Valanyr obtained!')
+        elif items_dictionary['motes'] >= 250:
+            legendary_is_obtained = True
+            items_dictionary['motes'] -= 250
+            print('Dragonwrath obtained!')
+        if legendary_is_obtained:
+            break
+    if legendary_is_obtained:
         break
-    name, phone = current_input.split('-')
-    phonebook[name] = phone
-for check in range(int(current_input)):
-    searched_name = input()
-    if searched_name in phonebook.keys():
-        found_number = phonebook[searched_name]
-        print(f"{searched_name} -> {found_number}")
-    else:
-        print(f"Contact {searched_name} does not exist.")
+
+    current_items = input().split()
+
+for material, quantity in items_dictionary.items():
+    print(f"{material}: {quantity}")
+
+
 
