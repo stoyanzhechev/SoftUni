09eea6487b4036ed43b8a877cb249b5dc09b757d
stoyanzhechev <marbulk@gmail.com>
2023-02-10 15:05:57 +0200

    initial commit

--- Programming Fundamentals - Python/temp.py
+++ Programming Fundamentals - Python/temp.py
@@ -1,8 +1,9 @@
-from math import pi
+deposit_amount = float(input())
+deposit_duration = int(input())
+annual_interest_rate = float(input())
 
+final_amount = deposit_amount + deposit_duration * (deposit_amount * (annual_interest_rate / 100)) / 12
 
-rad = float(input())
-deg = (rad * 180) / pi
+print(final_amount)
 
-print(deg)
 
