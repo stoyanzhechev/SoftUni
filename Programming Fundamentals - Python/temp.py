3937b0dc019ef85668bb08bd143ae0e33db32821
stoyanzhechev <marbulk@gmail.com>
2023-02-10 14:58:38 +0200

    initial commit

--- Programming Fundamentals - Python/temp.py
+++ Programming Fundamentals - Python/temp.py
@@ -1,10 +1,4 @@
-greening_area = float(input())
-price_per_sqm = 7.61
+usd_amount = float(input())
+bgn_amount = usd_amount * 1.79549
 
-total_price = greening_area * price_per_sqm
-discount = 0.18 * total_price
-discounted_price = 0.82 * total_price
-
-print(f"The final price is: {discounted_price:.2f} lv.")
-print(f"The discount is: {discount} lv.")
-
+print(bgn_amount)
