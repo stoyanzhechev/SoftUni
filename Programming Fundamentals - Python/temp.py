98a7d2410948d4d9a726293a2da4b352fda1e5fb
stoyanzhechev <marbulk@gmail.com>
2023-02-10 17:46:34 +0200

    initial commit

--- Programming Fundamentals - Python/temp.py
+++ Programming Fundamentals - Python/temp.py
@@ -1,21 +1,17 @@
-nylon_quantity = int(input())
-paint_quantity = int(input())
-detergent_quantity = int(input())
-hours_needed = int(input())
+chicken_count = int(input())
+fish_count = int(input())
+veggie_count = int(input())
 
-nylon_price = 1.50
-paint_price = 14.50
-detergent_price = 5.00
-plastic_bags_price = 0.40
+chicken_price = 10.35
+fish_price = 12.40
+veggie_price = 8.15
 
-nylon_cost = (nylon_quantity + 2) * nylon_price
-paint_cost = (paint_quantity * paint_price) * 1.10
-detergent_cost = detergent_quantity * detergent_price
+chicken_cost = chicken_count * chicken_price
+fish_cost = fish_count * fish_price
+veggie_cost = veggie_count * veggie_price
+dessert_cost = (chicken_cost + fish_cost + veggie_cost) * 0.20
+delivery_cost = 2.50
 
-total_cost = nylon_cost + paint_cost + detergent_cost + plastic_bags_price
-labor_price_per_hour = total_cost * 0.30
-total_labor_cost = hours_needed * labor_price_per_hour
-
-grand_total_cost = total_cost + total_labor_cost
+grand_total_cost = chicken_cost + fish_cost + veggie_cost + dessert_cost + delivery_cost
 
 print(grand_total_cost)
