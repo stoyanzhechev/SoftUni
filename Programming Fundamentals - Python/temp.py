f97af9dd7bfb6c31ae7b62057dbc42a375481cbd
stoyanzhechev <marbulk@gmail.com>
2023-08-22 15:51:46 +0300

    initial commit

--- Programming Fundamentals - Python/temp.py
+++ Programming Fundamentals - Python/temp.py
@@ -1,17 +1,5 @@
-chicken_count = int(input())
-fish_count = int(input())
-veggie_count = int(input())
+degree = float(input())
 
-chicken_price = 10.35
-fish_price = 12.40
-veggie_price = 8.15
+if degree >= 5:
+    print('Excellent!')
 
-chicken_cost = chicken_count * chicken_price
-fish_cost = fish_count * fish_price
-veggie_cost = veggie_count * veggie_price
-dessert_cost = (chicken_cost + fish_cost + veggie_cost) * 0.20
-delivery_cost = 2.50
-
-grand_total_cost = chicken_cost + fish_cost + veggie_cost + dessert_cost + delivery_cost
-
-print(grand_total_cost)
